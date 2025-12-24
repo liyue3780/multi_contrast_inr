63227fea2de7791ae26eca23d105edd90ec0f891
Yue <liyue3780@gmail.com>
2025-12-23 22:13:37 -0500

    Improve convert_seg_to_continuous utility function
    - Move convert_seg_to_continuous from dataset.py to dataset_utils.py
    - Use dynamic temporary directory in parent path instead of hardcoded absolute path
    - Generate distinguishable output filenames to avoid conflicts with file finding logic
    - Remove docstring as requested

--- dataset_utils.py
+++ dataset_utils.py
@@ -5,6 +5,9 @@ from tqdm import tqdm
 import torch.nn.functional as F
 import nibabel as nib
 from sklearn.preprocessing import MinMaxScaler
+import os
+import SimpleITK as sitk
+from os.path import join
 
 # from Tancik et al.:
 # https://github.com/tancik/fourier-feature-networks/blob/master/Experiments/3d_MRI.ipynb
@@ -69,3 +72,68 @@ def get_image_coordinate_grid_nib(image: nibabel.Nifti1Image):
         'coordinates_norm': torch.tensor(coordinates_arr_norm, dtype=torch.float32),
     }
     return image_dict
+
+
+def get_seg_coordinate_grid_nib(image: nibabel.Nifti1Image):
+    seg_dict = get_image_coordinate_grid_nib(image)
+
+    img_data = image.get_fdata()
+    label = list(img_data.flatten())
+    label_arr = np.array(label, dtype=np.float32)
+    label_one_hot = seg2onehot(label_arr)
+    seg_dict['label_one_hot'] = torch.tensor(label_one_hot)
+
+    label_num = len(np.unique(label_arr))
+    seg_dict['label_num'] = label_num
+
+    return seg_dict
+
+
+def seg2onehot(seg_array):
+    seg_array = seg_array.squeeze()
+    unique_classes = np.unique(seg_array)
+    class_to_index = {c: i for i, c in enumerate(unique_classes)}
+    num_classes = len(unique_classes)
+    mapped = np.vectorize(class_to_index.get)(seg_array)
+    one_hot = np.eye(num_classes)[mapped]  # shape: (N, num_classes)
+
+    return one_hot.astype(np.uint8)
+
+
+def pad_tensor_columns(x, target_cols, pad_value=-1):
+    current_cols = x.shape[1]
+    if current_cols >= target_cols:
+        return x  # no padding needed
+    pad_cols = target_cols - current_cols
+    pad = torch.full((x.shape[0], pad_cols), pad_value, dtype=x.dtype, device=x.device)
+    return torch.cat([x, pad], dim=1)
+
+
+def convert_seg_to_continuous(seg_path):
+    seg_image = sitk.ReadImage(seg_path)
+    seg_array = sitk.GetArrayFromImage(seg_image)
+    unique_seg_value = np.unique(seg_array)
+    sorted_a = np.sort(unique_seg_value)
+    
+    parent_dir = os.path.dirname(seg_path)
+    tmp_dir = join(parent_dir, 'tmp_continuous_labels')
+    if not os.path.exists(tmp_dir):
+        os.makedirs(tmp_dir, exist_ok=True)
+    
+    seg_name = os.path.basename(seg_path)
+    output_name = seg_name.replace('seg', 'continuous')
+    if 'continuous' not in output_name:
+        output_name = 'continuous_' + output_name
+    
+    output_path = join(tmp_dir, output_name)
+    
+    label_dict = {}
+    c3d_command = f'c3d {seg_path} -replace'
+    for ii, ele_ in enumerate(sorted_a):
+        label_dict[ii] = int(ele_)
+        c3d_command = c3d_command + f' {int(ele_)} {ii}'
+    c3d_command = c3d_command + f' -o {output_path}'
+    
+    os.system(c3d_command)
+    
+    return output_path, label_dict
